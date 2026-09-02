1eae98e03e054100ee2bda29375efd71d045207b
AnavadyaBCAA <anavadyamolu@gmail.com>
2026-09-02 10:34:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,105 @@
-<!DOCTYPE html>
-<html>
+// TODO 1:
+// Create a Recipe component.
 
-<head>
-    <title>React Card</title>
+function Recipe()
+{
 
-    <script src="https://unpkg.com/react@18/umd/react.development.js"></script>
-    <script src="https://unpkg.com/react-dom@18/umd/react-dom.development.js"></script>
+    return(
 
-    <script src="https://unpkg.com/@babel/standalone/babel.min.js"></script>
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-</head>
+        <div className="container">
+           <div className="row">
+   <div className="col-4">
 
-<body>
+            {/* Card 1 */}
+<div className="card" style={{width: "18rem;"}}>
+  <img className="card-img-top" src="image1.jpg" alt="CHICKEN NOODLES SOUP"/>
+  <div className="card-body">
+    <div className="card-body">
+  <h5 className="card-title">CHICKEN NOODLE SOUP</h5>
 
-<div id="root"></div>
+  <ul className="list-group list-group-flush"></ul>
+     <ul className="list-group list-group-flush">
+    <li className="list-group-item">2 Tablespoons extra virgin olive oil</li>
+    <li className="list-group-item">1 yellow onion</li>
+    <li className="list-group-item">2 medium carrots</li>
+    <li className="list-group-item">2 stalk celery</li>
+    <li className="list-group-item">8 cups chiken broth</li>
+    <li className="list-group-item">1(16 oz) package wide egg noodles</li>
+    <li className="list-group-item">1 cup cooked chiken</li>
+  </ul>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+</div>
 
-<script type="text/babel" src="index.js"></script>
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+</div></div>
 
 
-</body>
 
-</html>
+<div className="col-4">
+
+<div className="card" style={{width: "18rem;"}}>
+  <img className="card-img-top" src="image2.jpg" alt="CURRIED EGG SALAD"/>
+  <div className="card-body">
+    <div className="card-body">
+  <h5 className="card-title">CURRIED EGG SALAD</h5>
+
+  <ul className="list-group list-group-flush"></ul>
+     <ul className="list-group list-group-flush">
+    <li className="list-group-item">12 hard boiled eggs</li>
+    <li className="list-group-item">1/2</li>
+    <li className="list-group-item">2 medium carrots</li>
+    <li className="list-group-item">2 stalk celery</li>
+    <li className="list-group-item">8 cups chiken broth</li>
+    <li className="list-group-item">1(16 oz) package wide egg noodles</li>
+    <li className="list-group-item">1 cup cooked chiken</li>
+  </ul>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+</div>
+</div>
+</div>
+
+
+
+<div className="col-4">
+
+            {/* Card 3 */}
+<div className="card" style={{width: "18rem;"}}>
+  <img className="card-img-top" src="image3.jpg" alt="OAT CLUSTERS"/>
+  <div className="card-body">
+    <div className="card-body">
+  <h5 className="card-title">OAT CLUSTERS</h5>
+
+  <ul className="list-group list-group-flush"></ul>
+     <ul className="list-group list-group-flush">
+    <li className="list-group-item">12 hard boiled eggs</li>
+    <li className="list-group-item">1/2</li>
+    <li className="list-group-item">2 medium carrots</li>
+    <li className="list-group-item">2 stalk celery</li>
+    <li className="list-group-item">8 cups chiken broth</li>
+    <li className="list-group-item">1(16 oz) package wide egg noodles</li>
+    <li className="list-group-item">1 cup cooked chiken</li>
+  </ul>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+</div>
+
+</div></div></div></div>
+
+    );
+
+  }
+
+// TODO 2:
+// Create the React Root
+
+const root = ReactDOM.createRoot(
+    document.getElementById("root")
+);
+
+// TODO 3:
+// Render the Recipe component.
+
+root.render(
+    <Recipe />
+);
